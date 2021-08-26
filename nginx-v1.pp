cf2070a259b7ced29f100d989f84c092563b818b
Zineb Ben <zineb.ben125@gmail.com>
2021-08-26 15:43:57 +0000

    manifest

---
+++ nginx-v1.pp
@@ -0,0 +1,14 @@
+# nginx-v1
+package { 'nginx':
+  ensure => 'present',
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+}
+
+file { '/usr/share/nginx/html/index.html':
+  content => "Welcome to nginx-v1.pp By Zaillnebe on puppet server",
+}
+
